15252eb1702500b4c34cf6e61c03c88facccf151
IbrohimToxtasinov <toxtasinovibrohim976@gmail.com>
2022-12-05 10:06:06 +0500

    added: architectura created

---
+++ lib/data/model/company_models.dart
@@ -0,0 +1,17 @@
+// class CompanyModels {
+//   final int id;
+//   final String carModel;
+//   final String averagePrice;
+//   final String logo;
+//   final int establishedYear;
+
+//   CompanyModels({
+//     required this.id,
+//     required this.carModel,
+//     required this.averagePrice,
+//     required this.logo,
+//     required this.establishedYear,
+//   });
+
+//   CompanyModels.fromJson
+// }
